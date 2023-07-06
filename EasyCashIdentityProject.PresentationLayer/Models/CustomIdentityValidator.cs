fbd675b5449097f661f94593d9ac50777fac7763
Tolunay <88236341+TolunayYilmaz@users.noreply.github.com>
2023-07-06 15:01:47 +0300

    Added Custom Identity Validator

---
+++ EasyCashIdentityProject.PresentationLayer/Models/CustomIdentityValidator.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace EasyCashIdentityProject.PresentationLayer.Models
+{
+    public class CustomIdentityValidator:IdentityErrorDescriber
+    {
+
+        //Arayüze yansıyan hataları override ederek Türkçe dil desteği sunuldu.
+        public override IdentityError PasswordTooShort(int length)
+        {
+            return new IdentityError()
+            {
+                Code = "PasswordTooShort",
+                Description=$"Parola en az {length} karakter olmalıdır"
+            };
+        }
+        public override IdentityError PasswordRequiresUpper()
+        {
+            return new IdentityError()
+            {
+                Code = "PasswordRequiresUpper",
+                Description = "Parolada en az 1 büyük karakter olmalıdır"
+            };
+        }
+        public override IdentityError PasswordRequiresLower()
+        {
+            return new IdentityError()
+            {
+                Code = "PasswordRequiresLower",
+                Description = "Parolada en az 1 küçük karakter olmalıdır"
+            };
+        }
+        public override IdentityError PasswordRequiresDigit()
+        {
+            return new IdentityError()
+            {
+                Code = "PasswordRequiresDigit",
+                Description = "Parolada en az 1 tane rakam olmalıdır."
+            };
+        }
+        public override IdentityError PasswordRequiresNonAlphanumeric()
+        {
+            return new IdentityError()
+            {
+                Code = "PasswordRequiresNonAlphanumeric",
+                Description = "Parolada en az 1 tane sembol olmalıdır."
+            };
+        }
+    }
+}
